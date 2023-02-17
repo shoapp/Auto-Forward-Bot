a17057c7babea3000fe2098f0e225dc91c93fcfa
shoapp <125400285+shoapp@users.noreply.github.com>
2023-02-17 15:05:25 +0500

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,13 +1,10 @@
 class Translation(object):
 
       # Start text
-      START = "Hi there! I am a channel auto forward bot. I can forward new messages & Media from one channel to another.\n\n• Source code:<b/> <b><a href='https://github.com/0AIB/Auto-Forward-Bot'>Auto Forward Bot</a><b/>"             
+      START = "Hi there! I am a channel auto forward bot. I can forward new messages & Media from one channel to another."             
 
       #About text
       ABOUT = """
 <b>📝 Language:</b> Python 3
 <b>🧰 Framework:</b> Pyrogram
-<b>👨‍💻 Developer:</b> @LxChatGroup
-<b>📢 Channel:</b> @DFF_UPDATES
-<b>👥 Group:</b> @Hollywood_0980
-<b>🌐Source Code:</b> https://github.com/0AIB/Auto-Forward-Bot"""
+
